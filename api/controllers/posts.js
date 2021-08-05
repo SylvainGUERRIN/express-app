3604ea23fc08109f4068e9d58814185d32446302
sylvain <sguerrin549@gmail.com>
2021-08-05 19:07:36 +0200

    add routes and controller for posts and index.js in api

---
+++ api/controllers/posts.js
@@ -0,0 +1,48 @@
+import express from 'express';
+//import mongoose from 'mongoose';
+
+//import PostMessage from "../models/postMessage.js";
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => {
+    //res.send('This works !')
+    //try{
+        //const postMessages = await PostMessage.find()
+        //console.log(postMessages)
+        //res.status(200).json(postMessages)
+    //} catch (error) {
+        //res.status(404).json({message: error.message})
+    //}
+}
+
+export const createPost = async (req, res) => {
+    //const { title, message, selectedFile, creator, tags } = req.body;
+
+    //const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    //res.send('Post creation')
+    //const post = req.body
+    //const newPost = new PostMessage(post)
+
+    //try{
+        //await newPostMessage.save()
+        //res.status(201).join(newPostMessage)
+    //} catch (error) {
+        //res.status(409).json({message: error.message})
+    //}
+}
+
+export const updatePost = async (req, res) => {
+
+    //const { id: _id } = req.params
+    //const post = req.body
+
+    //if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+    //const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true})
+
+    //res.json(updatedPost)
+
+}
+
+export default router
