1f546f80a24bfb194252937c0a4d371ad34d2dc4
sylvain <sguerrin549@gmail.com>
2021-08-13 22:13:40 +0200

    add signup system

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,10 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
-import User from '../models/user'
+import User from '../models/user.js'
+
+//replace test with secret token in env file
+const secret = 'test'
 
 export const signin = async (req, res) => {
     const { email, password } = req.body
@@ -13,8 +16,7 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
         if(!isPasswordCorrect) return res.status(400).json({ message : "Credentials aren't correct."})
 
-        //replace test with secret token in env file
-        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, 'test', { expiresIn: '1h' })
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, secret, { expiresIn: '1h' })
 
         res.status(200).json({ result: existingUser, token})
     } catch (error) {
@@ -24,5 +26,23 @@ export const signin = async (req, res) => {
 }
 
 export const signup = async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
 
+    try {
+        const oldUser = await User.findOne({ email });
+
+        if (oldUser) return res.status(400).json({ message: "User already exists" });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+
+        const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+
+        res.status(201).json({ result, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+
+        console.log(error);
+    }
 }
