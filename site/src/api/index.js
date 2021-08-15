c8dfd20599916d13c803b2485fd7bb996c441d28
sylvain <sguerrin549@gmail.com>
2021-08-15 16:55:45 +0200

    add navbar logic when user is connected

--- site/src/api/index.js
+++ site/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = '/posts';
 
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
--- site/src/components/Navbar/Navbar.js
+++ site/src/components/Navbar/Navbar.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {AppBar, Typography, Toolbar, Button, Avatar} from "@material-ui/core";
-import { Link } from "react-router-dom"
-import memories from "../../images/memories.png";
+import { Link, useHistory, useLocation } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import decode from 'jwt-decode'
 
+import memories from "../../images/memories.png"
+import * as actionType from '../../constants/actionTypes'
 import useStyles from "./styles"
 
 const Navbar = () => {
-    const classes = useStyles()
-    const user = null
+    //const classes = useStyles()
+    //const user = null
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const history = useHistory();
+    const classes = useStyles();
+
+    const logout = () => {
+        dispatch({ type: actionType.LOGOUT });
+
+        history.push('/auth');
+
+        setUser(null);
+    };
+
+    useEffect(() => {
+        const token = user?.token;
+
+        if (token) {
+            const decodedToken = decode(token);
+
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
